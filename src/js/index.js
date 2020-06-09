031e231bdf60fbe191eb9429dacf4294edd72fd3
Thomas Pinaud <pinaudt@gmail.com>
2020-06-09 16:01:55 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import test from './test.js';
+
+addEventListener('DOMContentLoaded', () => {
+    test();
+});
---
+++ src/js/test.js
@@ -0,0 +1,7 @@
+import {html, render} from 'uhtml';
+
+export default () => {
+
+    render(document.body, html`<h1>Ready to build things</h1>`);
+
+};
